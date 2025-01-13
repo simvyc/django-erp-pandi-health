9eb78e5203c8135ea923a69ae7f6674bc487e501
Simona Vycaite <67691428+simvyc@users.noreply.github.com>
2025-01-13 21:06:01 +0200

    Update scripts.js

--- pandi/static/js/scripts.js
+++ pandi/static/js/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const nav = document.querySelector('.nav');
 
     window.addEventListener('scroll', function () {
-        if (window.scrollY > 50) { // Adjust threshold as needed
+        if (window.scrollY > 50) { 
             nav.classList.add('solid');
         } else {
             nav.classList.remove('solid');
